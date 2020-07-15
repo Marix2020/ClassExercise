d8678dc30cc0029be0ed607333784fdbdb2c5343
Marix2020 <maricelocampo888@gmail.com>
2020-07-15 11:42:47 -0400

    Changes were made by M

---
+++ GITExerciseNew/src/main/java/local/added/HelloGITHub.java
@@ -0,0 +1,12 @@
+package local.added;
+
+public class HelloGITHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java with GITHub");
+
+	}
+
+}
